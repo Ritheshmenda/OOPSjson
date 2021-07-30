a5c6619167d92580da62dc916b9ab2db83978c81
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-30 09:06:14 +0530

    [Rithesh]added:inventoryjson

---
+++ OOPSjson/Program.cs
@@ -0,0 +1,15 @@
+﻿using ObjectOrientedProgram.InventoryManagement;
+using System;
+
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        const string INVENTORY_JSON = @"C:\Users\Rithesh\source\repos\Object oriented programming\InventoryManagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            InventoryMain main = new InventoryMain();
+            main.DisplayData(INVENTORY_JSON);
+        }
+    }
+}
